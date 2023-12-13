7c481b5e45053b8ae148ba10e0727d9f8fb82217
Alexandr Belov <b.e.1.o.v@yandex.ru>
2023-12-13 03:01:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from treys import Card, Evaluator
+from fastapi import FastAPI
+
+app = FastAPI()
+evaluator = Evaluator()
+
+@app.get("/poker/check_it_please/{hand}")
+def get_combination(hand: str):
+    data = hand.split("-")
+    board = []
+    hand = [Card.new(card) for card in data]
+    score = evaluator.evaluate(hand, board)
+    handClass = evaluator.get_rank_class(score)
+    return handClass
